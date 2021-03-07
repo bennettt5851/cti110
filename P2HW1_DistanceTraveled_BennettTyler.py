e54b524cd38ec6af4e526e5cf6cba2654f6e4dd3
bennettt5851 <80208362+bennettt5851@users.noreply.github.com>
2021-03-07 10:46:37 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_BennettTyler.py
@@ -0,0 +1,25 @@
+# This program will calculate distance traveled
+# 3/7/21
+# CTI-110 P2HW1 - DistanceTraveled
+# Tyler Bennett
+#
+
+print('Enter cars speed:', end='')
+speed = int(input())
+print('')                          
+print('Enter time traveled:', end='')
+time = int(input())
+print('')
+print('Speed entered:', speed)
+print('Time entered:', time)
+print('')
+print('Distance Traveled:', speed * time, 'miles')
+
+# Display "Enter cars speed:" on screen
+# Collect user input for "speed"
+# Display "Enter time traveled"
+# Collect user input for "time"
+# Multiply the time * speed
+# Display the results as "Distance Traveled"
+
+        
